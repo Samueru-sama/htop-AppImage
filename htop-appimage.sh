ffc570c277989954c07936249caaf7e21304b4e9
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-05-22 04:00:45 -0400

    Update htop-appimage.sh

--- htop-appimage.sh
+++ htop-appimage.sh
@@ -9,7 +9,7 @@ if [ -z "$APP" ]; then exit 1; fi
 mkdir -p ./"$APP/$APPDIR" && cd ./"$APP/$APPDIR" || exit 1
 
 # DOWNLOAD AND BUILD HTOP
-CURRENTDIR="$(readlink -f "$(dirname "$0")")" # DO NOT MOVE THIS
+CURRENTDIR="$(dirname "$(readlink -f "$0")")" # DO NOT MOVE THIS
 version=$(wget -q https://api.github.com/repos/$SITE/releases -O - | sed 's/[()",{}]/ /g; s/ /\n/g' | grep -o 'https.*releases.*htop.*tar.xz' | head -1)
 wget "$version" && tar fx ./*tar* && cd ./htop* && ./autogen.sh && ./configure --prefix="$CURRENTDIR" --enable-sensors --enable-static \
 && make && make install && cd .. && rm -rf ./htop* ./*tar* || exit 1
